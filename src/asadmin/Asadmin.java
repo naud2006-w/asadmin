d683daab56248ad93607bd74958604be5e831a13
naud2006-w <naud@inbox.ru>
2015-04-27 10:25:39 +0300

    главная форма приложения
    настройка главной формы

--- src/asadmin/Asadmin.java
+++ src/asadmin/Asadmin.java
@@ -15,9 +15,11 @@ public class Asadmin {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        System.out.print("dfgsfgfg");
+        
+        Mainwindow mw = new Mainwindow();
+        
+        mw.show();
         
     }
     
